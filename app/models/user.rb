f4dfa737021f72f40719eb1a0da3294157f9d713
EC2 Default User <ec2-user@ip-172-31-7-97.ap-northeast-1.compute.internal>
2023-02-08 13:59:16 +0000

    user_model編集23/02/08

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   generate_public_uid
 
+  has_many :posts,              dependent: :destroy
+  has_many :post_comments,      dependent: :destroy
+  has_many :post_favorites,     dependent: :destroy
+  has_many :cooks,              dependent: :destroy
+  has_many :cook_comments,      dependent: :destroy
+  has_many :cook_favorites,     dependent: :destroy
+  has_many :tranings,           dependent: :destroy
+
+
+
   # フォローする側から中間テーブルへのアソシエーション
   has_many :follows, foreign_key: :following_id
   # フォローされる側から中間テーブルへのアソシエーション
@@ -14,7 +24,7 @@ class User < ApplicationRecord
   has_many :followings, through: :follows, source: :follower
   # フォローされる側からフォローしているユーザを取得する
   has_many :followers, through: :reverse_of_follows, source: :following
-  
+
   # フォローしているか判定
   def is_followed_by?(user)
     reverse_of_follows.find_by(following_id: user.id).present?
